55e059208f7af8d39007ca262b8db1ddd4f1103b
Dominik Gander <dominik.gander@1up.io>
2018-01-25 17:57:20 +0100

    API standalone-file added

---
+++ src/API.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+error_reporting(1);
+
+// Set the script name
+define('TL_SCRIPT', 'audit.php');
+
+// Initialize the system
+define('TL_MODE', 'BE');
+
+$possibleLockfiles[] = [
+    '../../composer.lock',
+    '../../../../../../../composer.lock',
+    '../../../../../../composer.lock',
+    $_SERVER['DOCUMENT_ROOT'] . '/composer.lock'
+];
+
+$possibleLockfiles[] = [
+    '../../composer/composer.lock',
+    '../../../../../../../composer/composer.lock',
+    '../../../../../../composer/composer.lock',
+    $_SERVER['DOCUMENT_ROOT'] . '/composer/composer.lock'
+];
+
+
+foreach ([
+             '../../initialize.php',
+             '../../../../../../../system/initialize.php',
+             '../../../../../../system/initialize.php',
+             $_SERVER['DOCUMENT_ROOT'] . '/system/initialize.php'
+         ] as $script
+) {
+    if (file_exists($script)) {
+        require_once($script);
+        break;
+    }
+}
+
+if($GLOBALS['TL_CONFIG']['securityAdvisory_enableAPI']) {
+
+    if(isset($_GET['token']) && $_GET['token'] == $GLOBALS['TL_CONFIG']['securityAdvisory_APIKey']) {
+        $foundFiles = [];
+
+        foreach ($possibleLockfiles as $possibilities) {
+            foreach ($possibilities as $checkPath) {
+                if(file_exists($checkPath)) {
+                    $foundFiles[] = $checkPath;
+                    break;
+                }
+            }
+        }
+
+        if(count($foundFiles) > 0) {
+            $contents['locks'];
+            foreach ($foundFiles as $lockFile) {
+                $contents['locks'][] = file_get_contents($lockFile);
+            }
+
+            echo \GuzzleHttp\json_encode($contents);
+        } else {
+            die('No composer.lock files found on server');
+        }
+
+    } else {
+        die('Wrong API token');
+    }
+
+} else {
+    die('API not enabled');
+}
