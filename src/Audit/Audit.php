975f5b4eb356aa9086ca14a163d8140b707ccb53
wrflsckr <d.gander@hotmail.com>
2018-04-20 11:44:44 +0200

    Fix: appends issues instead of overwriting the previous ones (#5)

--- src/Audit/Audit.php
+++ src/Audit/Audit.php
@@ -16,18 +16,15 @@ class Audit
         // Decode and get it as an array instead of stdClass
         $response = json_decode($response, true);
 
-        $vulnerabilities = [];
         foreach ($response as $key => $vulnerability) {
             $data = [
                 'name' => $key,
                 'version' => $vulnerability['version'],
-                'advisories' => array_values($vulnerability['advisories']),
+                'advisories' => array_values($vulnerability['advisories'])
             ];
 
-            $vulnerabilities[] = $data;
+            $this->vulnerabilities[] = $data;
         }
-
-        $this->vulnerabilities = $vulnerabilities;
     }
 
     public function isVulnerable()
